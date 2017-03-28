9d258e406780c6638fa9b80d585a7648c9474fc5
priteshmehta <mehta.pritesh@gmail.com>
2017-03-28 13:32:15 +0800

    Sample Go program

---
+++ hello.go
@@ -0,0 +1,23 @@
+/*
+Go Example 
+*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"text/template"
+
+	"github.com/gorilla/mux"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
+
+)
+
+func main() {
+	fmt.Println("Hello World!");
+}
