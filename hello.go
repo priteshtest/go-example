72b0288186a8f4891826d7f86915035bb4495f3c
priteshmehta <mehta.pritesh@gmail.com>
2017-05-03 10:27:39 +0800

    imported github.com/ant0ine/go-json-rest

--- hello.go
+++ hello.go
@@ -11,9 +11,17 @@ import (
 	"os"
 	"os/exec"
 	"text/template"
-	"github.com/ant0ine/go-json-rest"
+	"github.com/ant0ine/go-json-rest/rest"
+        "log"
+        "net/http"
 )
 
 func main() {
 	fmt.Println("Hello World!");
+        api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
+		w.WriteJson(map[string]string{"Body": "Hello World!"})
+	}))
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
